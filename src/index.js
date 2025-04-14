34eb1de613d79adb6615309aa0f2800b8b482f06
vomolo <valentineomolo1@gmail.com>
2025-04-14 14:14:33 +0300

    Add collision detection function for game objects

--- src/index.js
+++ src/index.js
@@ -223,3 +223,15 @@ function checkCollisions() {
         }
     });
 }
+
+function checkCollision(obj1, obj2) {
+    const rect1 = obj1.getBoundingClientRect();
+    const rect2 = obj2.getBoundingClientRect();
+
+    return !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+    );
+}
